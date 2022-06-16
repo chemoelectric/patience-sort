0f0aebc57cbd53015522b57fb33b7895ac2bd9b9
Barry Schwartz <sortsmill@crudfactory.com>
2022-06-16 13:20:22 -0500

    Initial commit.

---
+++ patience-sort.h
@@ -0,0 +1,29 @@
+/*
+  Copyright © 2022 Barry Schwartz
+
+  This program is free software: you can redistribute it and/or
+  modify it under the terms of the GNU General Public License, as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received copies of the GNU General Public License
+  along with this program. If not, see
+  <https://www.gnu.org/licenses/>.
+*/
+
+#include <stddef.h>
+
+void patience_sort (void *base, size_t nmemb, size_t size,
+                    int (*compar) (const void *, const void *),
+                    void *result);
+
+void patience_sort_r (void *base, size_t nmemb, size_t size,
+                      int (*compar) (const void *, const void *,
+                                     void *),
+                      void *arg,
+                      void *result);
